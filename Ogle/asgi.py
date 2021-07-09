ea129881b51c50286155203397d70b984992ee84
Yazek13 <fedorov_d_l@mail.ru>
2021-07-09 23:04:30 +1000

    start

---
+++ Ogle/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Ogle project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ogle.settings')
+
+application = get_asgi_application()
